9c41cd62d62eb2c99256a52aacdc97c22c9e758b
Yihui Xie <xie@yihui.name>
2018-09-27 17:19:33 -0500

    the output format html_paged()

---
+++ R/paged.R
@@ -0,0 +1,39 @@
+#' Create a paged HTML document suitable for printing
+#'
+#' This is an output format based on
+#' \code{bookdown::\link[bookdown]{html_document2}} (which means you can use
+#' those Markdown features added by \pkg{bookdown}). The HTML output document is
+#' split into multiple pages via a JavaScript library \pkg{paged.js}. These
+#' pages contain elements commonly seen in PDF documents, such as page numbers
+#' and running headers.
+#' @param ... Arguments passed to \code{bookdown::html_document2}.
+#' @param css A character vector of CSS file paths. If a path does not contain
+#'   the \file{.css} extension, it is assumed to be a built-in CSS file. For
+#'   example, \code{default-fonts} means the file
+#'   \code{pagedown:::pkg_resource('css', 'default-fonts.css')}. To see all
+#'   built-in CSS files, run \code{pagedown:::list_css()}.
+#' @param theme The Bootstrap theme. By default, Bootstrap is not used.
+#' @param template The path to the Pandoc template to convert Markdown to HTML.
+#' @return An R Markdown output format.
+#' @export
+html_paged = function(
+  ..., css = c('default-fonts', 'default'), theme = NULL,
+  template = pkg_resource('html', 'default.html')
+) {
+  css2 = grep('[.]css$', css, value = TRUE, invert = TRUE)
+  css  = setdiff(css, css2)
+  check_css(css2)
+  html_document2 = function(..., extra_dependencies = list()) {
+    bookdown::html_document2(..., extra_dependencies = c(
+      extra_dependencies, pagedjs_dependency(paste0(css2, '.css'))
+    ))
+  }
+  html_document2(..., css = css, theme = theme, template = template)
+}
+
+pagedjs_dependency = function(css = NULL) {
+  list(htmltools::htmlDependency(
+    'paged', packageVersion('pagedown'), src = pkg_resource(),
+    script = 'js/paged.js', stylesheet = file.path('css', css), all_files = FALSE
+  ))
+}
---
+++ R/utils.R
@@ -0,0 +1,20 @@
+pkg_resource = function(...) {
+  system.file('resources', ..., package = 'pagedown', mustWork = TRUE)
+}
+
+list_css = function() {
+  css = list.files(pkg_resource('css'), '[.]css$', full.names = TRUE)
+  setNames(css, gsub('.css$', '', basename(css)))
+}
+
+check_css = function(css) {
+  valid = names(list_css())
+  if (length(invalid <- setdiff(css, valid)) == 0) return()
+  invalid = invalid[1]
+  maybe = sort(agrep(invalid, valid, value = TRUE))[1]
+  hint = if (is.na(maybe)) '' else paste0('; did you mean "', maybe, '"?')
+  stop(
+    '"', invalid, '" is not a valid built-in CSS filename', if (hint != "") hint else ".",
+    " Use `pagedown:::list_css()` to view all built-in CSS filenames.", call. = FALSE
+  )
+}
