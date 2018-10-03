893108b9b11b94d9ed60d27733871f004dfe6a11
Romain Lesur <romain.lesur@justice.gouv.fr>
2018-10-03 15:28:02 +0200

    use a function that returns a Promise

--- inst/resources/js/config.js
+++ inst/resources/js/config.js
@@ -1,23 +1,21 @@
 // Configuration script for paged.js
 
-function appendShortTitleSpans() {
-  Array.from(document.getElementsByClassName('level1')).forEach(div => {
-    var mainHeader = div.getElementsByTagName('h1')[0];
-    var mainTitle = mainHeader.textContent;
-    var runningTitle = 'shortTitle' in div.dataset ? div.dataset.shortTitle : mainTitle;
-    var runningHeader = document.createElement('span');
-    runningHeader.className = 'shorttitle';
-    runningHeader.innerText = runningTitle;
-    div.insertBefore(runningHeader, mainHeader);
+const appendShortTitleSpans = () => {
+  return new Promise((resolve, reject) => {
+    Array.from(document.getElementsByClassName('level1')).forEach(div => {
+      var mainHeader = div.getElementsByTagName('h1')[0];
+      var mainTitle = mainHeader.textContent;
+      var runningTitle = 'shortTitle' in div.dataset ? div.dataset.shortTitle : mainTitle;
+      var runningHeader = document.createElement('span');
+      runningHeader.className = 'shorttitle';
+      runningHeader.innerText = runningTitle;
+      div.insertBefore(runningHeader, mainHeader);
+    });
+    resolve();
   });
-}
+};
 
 window.PagedConfig = {
-  before: () => {
-    return new Promise((resolve, reject) => {
-      appendShortTitleSpans();
-      resolve();
-    })
-  },
+  before: appendShortTitleSpans,
   after: (flow) => { console.log("after", flow) },
 };
