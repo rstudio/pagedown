764e6052d9ffcb3effbec76f5aea22f568103eda
Romain Lesur <romain.lesur@gmail.com>
2018-10-06 23:45:40 +0200

    do not use forEach loop
    see for instance https://codeburst.io/javascript-async-await-with-foreach-b6ba62bbf404

--- inst/resources/js/config.js
+++ inst/resources/js/config.js
@@ -3,10 +3,12 @@
    The text content of these spans are reused for running titles (see default.css).
    Argument: level - An integer between 1 and 6.
 */
-const appendShortTitleSpans = (level) => {
-  return () => {
-    return new Promise((resolve, reject) => {
-      Array.from(document.getElementsByClassName('level' + level)).forEach(div => {
+function appendShortTitleSpans(level) {
+  return async () => {
+    var divs = Array.from(document.getElementsByClassName('level' + level));
+
+    function addSpan(div) {
+      return new Promise((resolve, reject) => {
         var mainHeader = div.getElementsByTagName('h' + level)[0];
         var mainTitle = mainHeader.textContent;
         var runningTitle = 'shortTitle' in div.dataset ? div.dataset.shortTitle : mainTitle;
@@ -14,11 +16,15 @@ const appendShortTitleSpans = (level) => {
         span.className = 'shorttitle' + level;
         span.innerText = runningTitle;
         div.insertBefore(span, mainHeader);
+        resolve();
       });
-      resolve();
-    });
+    }
+
+    for (const div of divs) {
+      await addSpan(div);
+    }
   };
-};
+}
 
 var appendShortTitles1 = appendShortTitleSpans(1);
 var appendShortTitles2 = appendShortTitleSpans(2);
