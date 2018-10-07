855d8e11893fc05505524039ce4ca598cba08a2c
Romain Lesur <romain.lesur@gmail.com>
2018-10-07 17:46:43 +0200

    remove markers on broken items (#5)

--- inst/resources/js/hooks.js
+++ inst/resources/js/hooks.js
@@ -9,11 +9,54 @@ class numberOl extends Paged.Handler {
   }
 
   afterRendered(pages) {
-      var orderedLists = document.getElementsByTagName('ol');
-      for (var list of orderedLists) {
-        list.start = list.firstElementChild.dataset.pagedownItemNum;
-      }
+    var orderedLists = document.getElementsByTagName('ol');
+    for (var list of orderedLists) {
+      list.start = list.firstElementChild.dataset.pagedownItemNum;
+    }
   }
 }
 
-Paged.registerHandlers(numberOl);
+// Hook for broken items: when a list item is broken, the marker appears on the
+// remaining content.
+// The following hook removes the marker.
+
+function hasItemParent(node) {
+  if (node.parentElement === null) {
+    return false;
+  } else {
+    if (node.parentElement.tagName === 'LI') {
+      return true;
+    } else {
+      return hasItemParent(node.parentElement);
+    }
+  }
+}
+
+var brokenItem = false;
+
+class removeMarkerOnBrokenItem extends Paged.Handler {
+  constructor(chunker, polisher, caller) {
+    super(chunker, polisher, caller);
+  }
+
+  afterPageLayout(pageFragment, page, breakToken) {
+    if (brokenItem === true) {
+      pageFragment.classList.add('broken-item');
+    }
+    if (typeof(breakToken) === "undefined") {
+      brokenItem = false;
+    } else {
+      brokenItem = breakToken.node.nodeName === "#text" && hasItemParent(breakToken.node);
+    }
+  }
+
+  afterRendered(pages) {
+    var brokenItems = document.querySelectorAll('.broken-item li:first-of-type');
+    for (var item of brokenItems) {
+      item.style.listStyleType = "none";
+    }
+  }
+}
+
+// Register hooks
+Paged.registerHandlers(numberOl, removeMarkerOnBrokenItem);
