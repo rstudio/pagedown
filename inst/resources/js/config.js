576ae6a83abd23ad0c39bda3835be31e9a61a884
Romain Lesur <romain.lesur@gmail.com>
2018-10-09 00:13:18 +0200

    add spans for leading dots (#3)

---
+++ inst/resources/js/config.js
@@ -0,0 +1,13 @@
+// Configuration script for paged.js
+
+// This function add spans for leading symbols.
+async function addLeadersSpans() {
+  var anchors = document.querySelectorAll('.toc a');
+  for (var a of anchors) {
+    a.innerHTML = a.innerHTML + '<span class="leaders"></span>';
+  }
+}
+
+window.PagedConfig = {
+  before: addLeadersSpans
+};
