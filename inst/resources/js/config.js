198398ba9d1356f35e617689bc940e61ae97dd84
Romain Lesur <romain.lesur@gmail.com>
2018-10-10 01:38:37 +0200

    do not use global variables

--- inst/resources/js/config.js
+++ inst/resources/js/config.js
@@ -1,42 +1,45 @@
 // Configuration script for paged.js
-/* A factory returning a function that appends short titles spans.
-   The text content of these spans are reused for running titles (see default.css).
-   Argument: level - An integer between 1 and 6.
-*/
-function appendShortTitleSpans(level) {
-  return async () => {
-    var divs = Array.from(document.getElementsByClassName('level' + level));
 
-    async function addSpan(div) {
-      var mainHeader = div.getElementsByTagName('h' + level)[0];
-      var mainTitle = mainHeader.textContent;
-      var runningTitle = 'shortTitle' in div.dataset ? div.dataset.shortTitle : mainTitle;
-      var span = document.createElement('span');
-      span.className = 'shorttitle' + level;
-      span.innerText = runningTitle;
-      mainHeader.insertAdjacentElement('afterend', span);
-      if (level == 1 && div.querySelector('.level2') === null) {
-        var span2 = document.createElement('span');
-        span2.className = 'shorttitle2';
-        span2.innerText = ' ';
-        span.insertAdjacentElement('afterend', span2);
+(function() {
+  /* A factory returning a function that appends short titles spans.
+     The text content of these spans are reused for running titles (see default.css).
+     Argument: level - An integer between 1 and 6.
+  */
+  function appendShortTitleSpans(level) {
+    return async () => {
+      var divs = Array.from(document.getElementsByClassName('level' + level));
+
+      async function addSpan(div) {
+        var mainHeader = div.getElementsByTagName('h' + level)[0];
+        var mainTitle = mainHeader.textContent;
+        var runningTitle = 'shortTitle' in div.dataset ? div.dataset.shortTitle : mainTitle;
+        var span = document.createElement('span');
+        span.className = 'shorttitle' + level;
+        span.innerText = runningTitle;
+        mainHeader.insertAdjacentElement('afterend', span);
+        if (level == 1 && div.querySelector('.level2') === null) {
+          var span2 = document.createElement('span');
+          span2.className = 'shorttitle2';
+          span2.innerText = ' ';
+          span.insertAdjacentElement('afterend', span2);
+        }
       }
-    }
 
-    for (const div of divs) {
-      await addSpan(div);
+      for (const div of divs) {
+        await addSpan(div);
+      }
+    };
+  }
+
+  var appendShortTitles1 = appendShortTitleSpans(1);
+  var appendShortTitles2 = appendShortTitleSpans(2);
+
+  window.PagedConfig = {
+    before: () => {
+      return Promise.all([
+        appendShortTitles1(),
+        appendShortTitles2()
+      ]);
     }
   };
-}
-
-var appendShortTitles1 = appendShortTitleSpans(1);
-var appendShortTitles2 = appendShortTitleSpans(2);
-
-window.PagedConfig = {
-  before: () => {
-    return Promise.all([
-      appendShortTitles1(),
-      appendShortTitles2()
-    ]);
-  }
-};
+})();
