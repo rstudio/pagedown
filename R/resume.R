2a5d06be3a38a616f670194af38e44a823f25684
Yihui Xie <xie@yihui.name>
2018-10-12 00:16:35 -0500

    forgot the ... argument

--- R/resume.R
+++ R/resume.R
@@ -2,10 +2,10 @@
 #'
 #' This output format is based on Min-Zhong Lu's HTML/CSS in the Github repo
 #' \url{https://github.com/mnjul/html-resume}.
-#' @param css,template See \code{\link{html_paged}()}.
+#' @param ...,css,template See \code{\link{html_paged}()}.
 #' @return An R Markdown output format.
 #' @export
-html_resume = function(css = 'resume', template = pkg_resource('html', 'resume.html')) {
+html_resume = function(..., css = 'resume', template = pkg_resource('html', 'resume.html')) {
   html_format(
     ..., css = css, template = template, theme = NULL,
     .dependencies = list(rmarkdown::html_dependency_font_awesome())
