05acdfe5aee9ed1ea06fe0b6494e779ddf97df03
Yihui Xie <xie@yihui.name>
2018-10-12 00:12:16 -0500

    an output format for resume in HTML

---
+++ R/resume.R
@@ -0,0 +1,13 @@
+#' Create a resume in HTML
+#'
+#' This output format is based on Min-Zhong Lu's HTML/CSS in the Github repo
+#' \url{https://github.com/mnjul/html-resume}.
+#' @param css,template See \code{\link{html_paged}()}.
+#' @return An R Markdown output format.
+#' @export
+html_resume = function(css = 'resume', template = pkg_resource('html', 'resume.html')) {
+  html_format(
+    ..., css = css, template = template, theme = NULL,
+    .dependencies = list(rmarkdown::html_dependency_font_awesome())
+  )
+}
