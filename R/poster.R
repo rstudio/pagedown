3a1d197aa93342c8d4895059c1167666e6d8b1a5
Yihui Xie <xie@yihui.name>
2018-10-29 13:27:02 -0500

    a poster format based on the ReLaXed style

---
+++ R/poster.R
@@ -0,0 +1,24 @@
+#' Create posters in HTML
+#'
+#' The output format \code{poster_relaxed()} is based on an example in the Github repo
+#' \url{https://github.com/RelaxedJS/ReLaXed-examples}.
+#' @param ...,css,template,number_sections See \code{\link{html_paged}()}.
+#' @references \url{https://rstudio.github.io/pagedown/poster-relaxed.html}
+#' @return An R Markdown output format.
+#' @export
+poster_relaxed = function(
+  ..., css = 'poster-relaxed', template = pkg_resource('html', 'poster-relaxed.html'),
+  number_sections = FALSE
+) {
+  html_format(
+    ..., css = css, template = template, theme = NULL, number_sections = number_sections
+  )
+}
+
+
+# TODO: most posters like https://www.overleaf.com/gallery/tagged/poster
+
+# https://www.overleaf.com/latex/templates/landscape-beamer-poster-template/vjpmsxxdvtqk
+poster_jacobs = function(..., css = 'poster-jacobs') {
+  poster_relaxed(..., css = css)
+}
