b8360d1cbb11e5488a0c7b86af173aa35bacc171
Yihui Xie <xie@yihui.name>
2018-11-23 13:48:08 -0600

    default number_sections = FALSE in html_resume()

--- R/resume.R
+++ R/resume.R
@@ -3,12 +3,15 @@
 #' This output format is based on Min-Zhong Lu's HTML/CSS in the Github repo
 #' \url{https://github.com/mnjul/html-resume}. See
 #' \url{https://pagedown.rbind.io/html-resume/} for an example.
-#' @param ...,css,template See \code{\link{html_paged}()}.
+#' @param ...,css,template,number_sections See \code{\link{html_paged}()}.
 #' @return An R Markdown output format.
 #' @export
-html_resume = function(..., css = 'resume', template = pkg_resource('html', 'resume.html')) {
+html_resume = function(
+  ..., css = 'resume', template = pkg_resource('html', 'resume.html'),
+  number_sections = FALSE
+) {
   html_format(
-    ..., css = css, template = template, theme = NULL,
+    ..., css = css, template = template, theme = NULL, number_sections = number_sections,
     .dependencies = list(rmarkdown::html_dependency_font_awesome())
   )
 }
