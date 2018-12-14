a69cea5f85b0b386e6d1904213a24b75a1b91cc7
Yihui Xie <xie@yihui.name>
2018-12-14 11:57:20 -0600

    redirect the netlify subdomain and http links to https://pagedown.rbind.io

--- tools/website.R
+++ tools/website.R
@@ -9,3 +9,8 @@ options(htmltools.dir.version = FALSE)
 for (f in list.files('.', '[.]Rmd$')) {
   rmarkdown::render(f, output_options = list(self_contained = FALSE))
 }
+
+writeLines(c(
+  'http://pagedown.netlify.com/* https://pagedown.rbind.io/:splat 301!',
+  'http://pagedown.rbind.io/*    https://pagedown.rbind.io/:splat 301!'
+), '_redirects')
