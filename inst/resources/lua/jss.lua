f843a1cf330e6b8de6d44e90ee25312ee4587e0e
Romain Lesur <romain.lesur@gmail.com>
2019-01-09 22:41:33 +0100

    use fallback values for missing template parameters

--- inst/resources/lua/jss.lua
+++ inst/resources/lua/jss.lua
@@ -1,10 +1,11 @@
 --[[
     A Pandoc filter for the Journal of Statistical Software template
-    This filter performs two actions:
-    1- It stores plain keywords in a pandoc variable named keywords-plain
-    2- It build the DOI from the volume and issue parameters and stores it in a variable named doi
+    This filter performs the following actions:
+    - store plain keywords in a pandoc variable named keywords-plain
+    - calculate the rank for each author
+    - build the DOI from the volume and issue parameters and store it in a variable named doi
+    - use fallback values for missing month, year, volume or issue
 --]]
-
 local isInteger = function(number)
   return math.floor(number) == number
 end
@@ -22,27 +23,55 @@ local padIssue = function(issue)
   return issueString
 end
 
-local buildDOI = function(volume, issue)
+local getDOI = function(volume, issue)
+  -- store a fallback DOI
+  local fallback = "10.18637/jss.v000.i00"
+
+  -- if volume or issue is missing, return the fallback DOI
+  if not volume or not issue then return fallback end
+
+  local volumeNumber = tonumber(pandoc.utils.stringify(volume))
+  local issueNumber = tonumber(pandoc.utils.stringify(issue))
+
+  -- if volume or issue cannot be coerced to a number, return the fallback DOI
+  if not volumeNumber or not issueNumber then return fallback end
+
+  -- if volume or issue is a number lesser than 1, return the fallback DOI
+  if volumeNumber < 1 or issueNumber < 1 then return fallback end
+
+  -- if volume or issue is not an integer, return the fallback DOI
+  if not isInteger(volumeNumber) or not isInteger(issueNumber) then return fallback end
+
+  -- build the DOI
   return "10.18637/jss.v" .. padVolume(volume) .. ".i" .. padIssue(issue)
 end
 
+
 Meta = function(meta)
-  ------------------------------------
-  --          Keywords              --
-  ------------------------------------
+  ---------------------------------------
+  --           Keywords                --
+  ---------------------------------------
   -- Test if there is one keyword:
   if not meta.keywords then error("At least one keyword must be supplied.") end
   -- Store plain keywords:
   local plainKeywords = {}
-  for i, v in ipairs(meta.keywords) do
-    plainKeywords[i] = pandoc.utils.stringify(v)
+
+  if meta.keywords.t == "MetaList" then
+    for i, v in ipairs(meta.keywords) do
+      plainKeywords[i] = pandoc.utils.stringify(v)
+    end
+  else
+    -- we have only one keyword
+    plainKeywords = {pandoc.utils.stringify(meta.keywords)}
   end
+
   meta["keywords-plain"] = plainKeywords
 
-  ------------------------------------
-  --             Author             --
-  ------------------------------------
+  ---------------------------------------
+  --              Author               --
+  ---------------------------------------
   local author = meta.author
+
   if author.t == "MetaInlines" then
     meta.author = {data = author, rank = "1"}
   else
@@ -51,28 +80,18 @@ Meta = function(meta)
     end
   end
 
-  -------------------------------------
-  -- Build DOI from volume and issue --
-  -------------------------------------
-  -- store a fallback DOI
-  meta.doi = "10.18637/jss.vxxx.iyy"
+  ----------------------------------------
+  --  Build DOI from volume and issue   --
+  ----------------------------------------
+  meta.doi = getDOI(meta.volume, meta.issue)
 
-  -- if volume or issue is missing, return the fallback DOI
-  if not meta.volume or not meta.issue then return meta end
+  ----------------------------------------
+  -- Fallback values for missing params --
+  ----------------------------------------
+  if not meta.month then meta.month = "MMMMMM" end
+  if not meta.year then meta.year = "YYYY" end
+  if not meta.volume then meta.volume = "VV" end
+  if not meta.issue then meta.issue = "II" end
 
-  local volumeNumber = tonumber(pandoc.utils.stringify(meta.volume))
-  local issueNumber = tonumber(pandoc.utils.stringify(meta.issue))
-
-  -- if volume or issue cannot be coerced to a number, return the fallback DOI
-  if not volumeNumber or not issueNumber then return meta end
-
-  -- if volume or issue is a number lesser than 1, return the fallback DOI
-  if volumeNumber < 1 or issueNumber < 1 then return meta end
-
-  -- if volume or issue is not an integer, return the fallback DOI
-  if not isInteger(volumeNumber) or not isInteger(issueNumber) then return meta end
-
-  -- build the DOI
-  meta.doi = buildDOI(volumeNumber, issueNumber)
   return meta
 end
