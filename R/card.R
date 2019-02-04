a6018457975acfcc2aacefc6021ef9774c105dab
Romain LESUR <RLesur@users.noreply.github.com>
2019-02-04 22:05:28 +0100

    [business_card] Multiple business cards (#56)
    * use an HTML template
    
    * wrap all cards in wrapper and duplicate styles in shadow DOMs
    
    * use flexbox (works well for a single page)
    
    * use a grid instead of a flexbox
    
    * keep the default options (one card/full page)
    
    * add a repeat variable
    
    * add a border for white theme
    
    * document new features of business_card
    
    * remove logo argument
    
    * allow markdown for card creation
    
    * improve comments in code
    
    * document markdown support for business card creation
    
    * [business_card] use top-level YAML variables
    
    * fix the example
    
    * roxygenize

--- R/card.R
+++ R/card.R
@@ -4,27 +4,15 @@
 #' \url{https://github.com/RelaxedJS/ReLaXed-examples}. See
 #' \url{https://pagedown.rbind.io/business-card/} for an example.
 #'
-#' @param logo Path or URL to a logo.
-#' @param width,height Width and height of the card.
-#' @param googlefonts Names of Google fonts to be loaded on the card page.
-#' @param mainfont Names of fonts to be used for the body text of the card.
 #' @return An R Markdown output format.
 #' @export
-#' @examples pagedown::business_card(googlefonts = 'Lato')
-business_card = function(
-  logo = NULL, width = '2in', height = '3in',
-  googlefonts = 'Montserrat', mainfont = googlefonts
-) {
+#' @examples pagedown::business_card()
+business_card = function() {
   rmarkdown::output_format(
     list(opts_chunk = list(echo = FALSE)),
     rmarkdown::pandoc_options('html', 'markdown', args = c(
       '--template', rmarkdown::pandoc_path_arg(pkg_resource('html', 'card.html')),
-      c(rbind('--variable', c(
-        paste0('logo=', logo),
-        paste0('googlefonts=', paste(googlefonts, collapse = '|')),
-        paste0('mainfont=', paste(mainfont, collapse = ', ')),
-        paste0('pagewidth=', width), paste0('pageheight=', height)
-      )))
+      '--variable', 'pagetitle=Business Card'
     ))
   )
 }
