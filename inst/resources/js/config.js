d6d8aad6989e86422f6c76c082c1e9d80da15041
Romain Lesur <romain.lesur@gmail.com>
2019-03-23 23:52:15 +0100

    force Chrome to redraw. This fixes #35 and resolves #46

--- inst/resources/js/config.js
+++ inst/resources/js/config.js
@@ -105,6 +105,12 @@
       await runMathJax();
     },
     after: () => {
+      // force redraw, see https://github.com/rstudio/pagedown/issues/35#issuecomment-475905361
+      // and https://stackoverflow.com/a/24753578/6500804
+      document.body.style.display = 'none';
+      document.body.offsetHeight;
+      document.body.style.display = '';
+
       // pagedownListener is a binder added by the chrome_print function
       // this binder exists only when chrome_print opens the html file
       if (window.pagedownListener) {
