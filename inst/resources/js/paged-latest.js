7f295033906a39c474be3d1f091fee75591d3602
Romain Lesur <romain.lesur@gmail.com>
2019-08-23 14:16:52 +0200

    load the latest version of Paged.js synchronously

--- inst/resources/js/paged-latest.js
+++ inst/resources/js/paged-latest.js
@@ -1,5 +1,6 @@
 {
   let script = document.createElement('script');
   script.src = 'https://unpkg.com/pagedjs/dist/paged.polyfill.js';
+  script.async = false;
   document.head.querySelector('script[src*="config.js"]').insertAdjacentElement('afterend', script);
 }
