312c63872b83fd15edde950c66a02f89a8950247
Romain LESUR <romain.lesur@gmail.com>
2019-08-28 17:45:12 +0200

    dynamically build CSS for front and back covers

--- inst/resources/js/config.js
+++ inst/resources/js/config.js
@@ -3,7 +3,35 @@
 (function() {
   // Retrieve previous config object if defined
   window.PagedConfig = window.PagedConfig || {};
-  const afterPaged = window.PagedConfig.after;
+  const {before: beforePaged, after: afterPaged} = window.PagedConfig;
+
+  // utils
+  const insertCSS = text => {
+    let style = document.createElement('style');
+		style.type = 'text/css';
+		style.appendChild(document.createTextNode(text));
+    document.head.appendChild(style);
+  };
+
+  // Support for front and back covers images CSS variables
+  const insertCSSForCover = type => {
+    const links = document.querySelectorAll('link[id^=' + type + ']');
+    if (!links.length) return;
+    const re = new RegExp(type + '-\\d+');
+    let text = ':root {--' + type + ': var(--' + type + '-1);';
+    for (const link of links) {
+      text += '--' + re.exec(link.id)[0] + ': url("' + link.href + '");';
+    }
+    text += '}';
+    insertCSS(text);
+  };
+
+  window.PagedConfig.before = async () => {
+    insertCSSForCover('front-cover');
+    insertCSSForCover('back-cover');
+
+    if (beforePaged) await beforePaged();
+  };
 
   window.PagedConfig.after = (flow) => {
     // force redraw, see https://github.com/rstudio/pagedown/issues/35#issuecomment-475905361
