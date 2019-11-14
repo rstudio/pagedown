fb3f7701b17f7d2218c57b9f938eb729954496df
Romain Lesur <romain.lesur@gmail.com>
2019-11-14 22:57:32 +0100

    test chrome_print() with an url, a local file and an Rmd file

--- tests/test-travis/test-chrome.R
+++ tests/test-travis/test-chrome.R
@@ -1,5 +1,32 @@
 library(testit)
 
+print_pdf = function(input) {
+  chrome_print(
+    input, tempfile(),
+    # use --no-sandbox with travis
+    # https://docs.travis-ci.com/user/chrome#sandboxing
+    extra_args = c('--disable-gpu', '--no-sandbox')
+  )
+}
+
+is_pdf = function(file) {
+  identical(readBin(file, 'raw', 5L), charToRaw('%PDF-'))
+}
+
 assert('find_chrome() finds Chrome executable', {
   (nzchar(find_chrome()))
+})
+
+assert('chrome_print() works with an url', {
+  (is_pdf(print_pdf('http://httpbin.org/html')))
+})
+
+assert('chrome_print() works with a local file path', {
+  r_faq_html = file.path(R.home('doc'), 'manual', 'R-FAQ.html')
+
+  (is_pdf(print_pdf(r_faq_html)))
+})
+
+assert('chrome_print() works with html_paged format', {
+  (is_pdf(print_pdf('test-chrome.Rmd')))
 })
