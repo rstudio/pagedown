9a39c3147e4d63b24ae447ee4fb8e66d0807844d
Christophe Dervieux <cderv@rstudio.com>
2021-11-09 13:04:34 +0100

    Use GHA instead of travis (#246)
    And test on 3 OS + several Pandoc version

--- tests/test-ci.R
+++ tests/test-ci.R
@@ -1,16 +1,16 @@
-# run tests on Travis (these tests depend on Chrome)
+# run tests on CI (these tests depend on Chrome)
 
 print_pdf = function(input, output = tempfile(), ...) {
   chrome_print(
     input, output,
     # use --no-sandbox with travis
     # https://docs.travis-ci.com/user/chrome#sandboxing
-    extra_args = c('--disable-gpu', '--no-sandbox'),
+    # extra_args = c('--disable-gpu', '--no-sandbox'),
     ...
   )
 }
 
 if (!is.na(Sys.getenv('CI', NA))) {
   options(pagedown.remote.maxattempts = 100L)
-  testit::test_pkg('pagedown', 'test-travis')
+  testit::test_pkg('pagedown', 'test-ci')
 }
