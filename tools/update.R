185cd46a7c473c3863129ca9a81b700a9e5ea78f
Felipe Curty do R. Pinto <felipecrp@gmail.com>
2021-11-14 13:08:11 -0300

    Update the paged.js to paged.polyfill.js

--- tools/update.R
+++ tools/update.R
@@ -1,4 +1,4 @@
 xfun::in_dir('tools', {
   # download paged.js
-  xfun::download_file('https://unpkg.com/pagedjs/dist/paged.polyfill.js', '../inst/resources/js/paged.js')
+  xfun::download_file('https://unpkg.com/pagedjs/dist/paged.polyfill.js', '../inst/resources/js/paged.polyfill.js')
 })
