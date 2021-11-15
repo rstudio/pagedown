35bad260c1aeda29c7a2a04fbad1aa3ba844e1ff
Felipe Curty <felipecrp@gmail.com>
2021-11-15 12:30:29 -0300

    Add JEST hook to run rmarkdown

--- tests/test-output/longtable.test.js
+++ tests/test-output/longtable.test.js
@@ -1,10 +1,16 @@
+const exec = require('child_process').execSync;
 
 describe("Long tables", () => {
     beforeAll(async () => {
         // TODO encapsulate the file path to make it easier to replicate in other tests
         const cwd = process.cwd();
         //const htmlFile = `file://{ $cwd }/tests/test-output/longtable.html`; // TODO not working
-        const htmlFile = 'file://' + cwd + '/tests/test-output/longtable.html';
+        const file = 'longtable'
+        const rmdFile = cwd + '/tests/test-output/' + file + '.Rmd';
+        const htmlFile = 'file://' + cwd + '/tests/test-output/' + file + '.html';
+
+        exec('Rscript -e "rmarkdown::render(\'' + rmdFile + '\')"');
+
         await page.goto(htmlFile, { waitUntil: "networkidle2" });
     });
 
