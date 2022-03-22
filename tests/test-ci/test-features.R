87772a260aabaae88b78f8a1ed529789b00678ec
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-03-22 15:02:25 +0100

    Update test to check LOT and LOF are filled correctly
    update template to add some syntax cases

--- tests/test-ci/test-features.R
+++ tests/test-ci/test-features.R
@@ -6,13 +6,13 @@ assert('Page breaks work', {
 })
 
 assert('lot / lof are inserted correctly', {
+  # This test is tied to skeleton.Rmd for html-paged template
+  # Check when skeleton.Rmd is modified
   if (xfun::loadable("xml2")) {
     dir.create(tmp_dir <- tempfile())
     xfun::in_dir(tmp_dir, {
       # use included template
       rmarkdown::draft("test.Rmd", "html-paged", "pagedown", edit = FALSE)
-      # add lot / lof
-      xfun::gsub_file("test.Rmd", pattern = "^---$", replacement = "---\nlot: true\nlof: true")
       res = rmarkdown::render("test.Rmd", quiet = TRUE)
     })
     content <- xml2::read_html(res, encoding = "UTF-8")
@@ -28,7 +28,11 @@ assert('lot / lof are inserted correctly', {
     main <- xml2::xml_find_all(content, "//div[contains(@class, 'main')]")
     divs <- xml2::xml_find_all(main, "div[@id]")
     (xml2::xml_attr(divs[1], 'id') == "LOT")
+    (length(xml2::xml_find_all(divs[1], 'ul/li/a[contains(@href, "md-tab")]')) == 1L)
+    (length(xml2::xml_find_all(divs[1], 'ul/li/a[contains(@href, "simple-tab")]')) == 1L)
     (xml2::xml_attr(divs[2], 'id') == "LOF")
+    (length(xml2::xml_find_all(divs[2], 'ul/li/a[contains(@href, "md-fig")]')) == 1L)
+    (length(xml2::xml_find_all(divs[2], 'ul/li/a[contains(@href, "simple-graphic")]')) == 1L)
 
     # Change titles
     xfun::in_dir(tmp_dir, {
