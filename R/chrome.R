bdaef4db89c8258d65a44c407561e6743906959f
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-09-29 11:13:12 +0200

    remove output file before running chrome printing
    and throw clear error if possibly locked

--- R/chrome.R
+++ R/chrome.R
@@ -172,6 +172,15 @@ chrome_print = function(
     if (missing(output) && !file.exists(input))
       output = xfun::with_ext(basename(gsub('[#?].*', '', url)), format)
     output2 = normalizePath(output, mustWork = FALSE)
+    # try to remove the file and throw a clear error if it still exists as it may be locked
+    if (!suppressWarnings(file.remove(output2)) && xfun::file_exists(output2)) {
+      stop(
+        sprintf("The file %s cannot be removed%s.",
+                sQuote(output),
+                if (format == "pdf") " (may be locked by a PDF reader)" else ""
+        )
+      )
+    }
     if (!dir.exists(d <- dirname(output2)) && !dir.create(d, recursive = TRUE)) stop(
       'Cannot create the directory for the output file: ', d
     )
