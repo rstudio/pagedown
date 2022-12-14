b8f9a0c3f6134b7015e93ac65621e1082de935ee
Yihui Xie <xie@yihui.name>
2022-12-13 21:00:45 -0600

    we no longer use travis

--- tests/test-ci.R
+++ tests/test-ci.R
@@ -1,13 +1,7 @@
 # run tests on CI (these tests depend on Chrome)
 
 print_pdf = function(input, output = tempfile(), ...) {
-  chrome_print(
-    input, output,
-    # use --no-sandbox with travis
-    # https://docs.travis-ci.com/user/chrome#sandboxing
-    # extra_args = c('--disable-gpu', '--no-sandbox'),
-    ...
-  )
+  chrome_print(input, output, ...)
 }
 
 if (!is.na(Sys.getenv('CI', NA))) {
