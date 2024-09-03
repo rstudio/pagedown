3b85d1585681ed07534fbb1f86de15a863f586b3
Yihui Xie <xie@yihui.name>
2024-09-03 10:43:17 -0500

    not sure why httpbin.org is failing

--- tests/test-ci/test-chrome.R
+++ tests/test-ci/test-chrome.R
@@ -32,7 +32,7 @@ assert('find_chrome() finds Chrome executable', {
 })
 
 assert('chrome_print() works with an url', {
-  (is_pdf(print_pdf('http://httpbin.org/html')))
+  (is_pdf(print_pdf('https://example.org')))
 })
 
 assert('chrome_print() works with a local file path', {
